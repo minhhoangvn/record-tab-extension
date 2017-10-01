5b3595c7098d0947af640a9ab387eeab1aa02064
U-MINH-HOANG\hnmin <hnminh@outlook.com>
2017-10-01 13:29:06 +0700

    init code

---
+++ assets/js/options.js
@@ -0,0 +1,41 @@
+/**
+ * Options module to manage the options
+ * @returns {{init: init}}
+ * @constructor
+ */
+function HubOptions() {
+    
+        var hubStorage = new HubStorage();
+    
+        /**
+         * Performs the UI bindings
+         */
+        function bindUI() {
+            $(document).on('click', '.save-token', function (e) {
+                e.preventDefault();
+    
+                hubStorage.persistFilters('.githunt_token');
+                $('.quote-item').html('Woohoo! Token saved, happy hunting.');
+            });
+        }
+    
+        return {
+    
+            /**
+             * Initializes the options page
+             */
+            init: function () {
+                var tokenPopulated = hubStorage.populateFilters('.githunt_token');
+                if (tokenPopulated) {
+                    $('.quote-item').html('Token already saved. Better go for the hunt!');
+                }
+    
+                bindUI();
+            }
+        };
+    }
+    
+    $(function () {
+        var hubOptions = new HubOptions();
+        hubOptions.init();
+    });
---
+++ content.js
@@ -0,0 +1,1 @@
+console.log("this in content", this);
